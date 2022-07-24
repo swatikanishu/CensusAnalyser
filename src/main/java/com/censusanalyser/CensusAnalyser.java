dc82bd9c65b5930532d99b4a02942716caf959cd
swatikanishu <nishuswatika@gmail.com>
2022-07-24 10:20:33 +0530

    Added:welcome message added

---
+++ src/main/java/com/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.censusanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to censusAnalyser program");
+
+    }
+}
+
